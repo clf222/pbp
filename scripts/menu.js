5c7da55b1b15e5c3b0596ebf9e1a9f08c1dc348a
Cindy Francies <clfrancies@comcast.net>
2017-09-26 19:54:30 -0400

    Style socials

--- scripts/menu.js
+++ scripts/menu.js
@@ -11,17 +11,31 @@ $( ".hamburger" ).hide();
 
 $( ".cross" ).click(function() {
 $( ".menu" ).slideToggle( "slow", function() {
+$(".menu").remove();
 $( ".cross" ).hide();
 $( ".hamburger" ).show();
 });
 });
-// this is a test not working
-$("ul li").click(function() {
-  $(".menu").fadeToggle("fast", function() {
-    $(".hamburger").show();
-  });
-  $
 });
+// this is a test not working
+// $("ul li").click(function() {
+//   $(".menu").fadeToggle("fast", function() {
+//     $(".hamburger").show();
+//   });
+//   $
+// $(function() {
+//     $('a[href*=#]:not([href=#])').click(function() {
+//         var target = $(this.hash);
+//         target = target.length ? target : $('[name=' + this.hash.substr(1) +']');
+//         if (target.length) {
+//             $('html,body').animate({
+//               scrollTop: target.offset().top
+//             }, 1000);
+//             return false;
+//         }
+//     });
+// });
+// });
 
 
 // need script so that when you click on #home, #about, #menu, #events, #location the following happens:
@@ -29,4 +43,4 @@ $("ul li").click(function() {
 // 2. header appears with .hamburger as visible
 // 3. go to the id of the clicked on menu item
 
-});
+// });
