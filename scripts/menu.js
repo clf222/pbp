0abe9ed9cdf582a6a32063ac7874644363d6744f
Cindy Francies <clfrancies@comcast.net>
2017-10-02 22:26:50 -0400

    working on h1 script

--- scripts/menu.js
+++ scripts/menu.js
@@ -16,31 +16,17 @@ $( ".cross" ).hide();
 $( ".hamburger" ).show();
 });
 });
+
+$(function () {
+        var isAdded = false;
+        $(window).resize(function () {
+            if (!isAdded && $(window).width() >= 768) {
+                 isAdded = true;
+                 $('#text').prepend('<h1>pop.<span>Bubble.</span>pop</h1>');
+            } else if (isAdded && $(window).width() <768) {
+                isAdded = false;
+                $('#text').remove();
+            }
+        });
+    });
 });
-// this is a test not working
-// $("ul li").click(function() {
-//   $(".menu").fadeToggle("fast", function() {
-//     $(".hamburger").show();
-//   });
-//   $
-// $(function() {
-//     $('a[href*=#]:not([href=#])').click(function() {
-//         var target = $(this.hash);
-//         target = target.length ? target : $('[name=' + this.hash.substr(1) +']');
-//         if (target.length) {
-//             $('html,body').animate({
-//               scrollTop: target.offset().top
-//             }, 1000);
-//             return false;
-//         }
-//     });
-// });
-// });
-
-
-// need script so that when you click on #home, #about, #menu, #events, #location the following happens:
-// 1.menu disappears
-// 2. header appears with .hamburger as visible
-// 3. go to the id of the clicked on menu item
-
-// });
