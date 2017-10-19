7ff68136996c644dd99a213acd7aabc873555543
Cindy Francies <clfrancies@comcast.net>
2017-10-19 16:11:26 -0400

    removing hamburger

--- scripts/menu.js
+++ scripts/menu.js
@@ -6,24 +6,24 @@ $( document ).ready(function() {
 
   });
 
-  $( ".cross" ).hide();
-  $( ".menu" ).hide();
-  $( ".hamburger" ).click(function() {
-      $( ".menu" ).slideToggle( "slow", function() {
-        $( ".hamburger" ).hide();
-        $( ".cross" ).show();
-      });
-  });
-function closeMenu() {
-  $( ".menu" ).slideToggle( "slow", function() {
-    $( ".cross" ).hide();
-    $( ".hamburger" ).show();
-  });
-}
-$( ".cross" ).click(closeMenu);
-
-
-$( ".menu li" ).click(closeMenu);
+//   $( ".cross" ).hide();
+//   $( ".menu" ).hide();
+//   $( ".hamburger" ).click(function() {
+//       $( ".menu" ).slideToggle( "slow", function() {
+//         $( ".hamburger" ).hide();
+//         $( ".cross" ).show();
+//       });
+//   });
+// function closeMenu() {
+//   $( ".menu" ).slideToggle( "slow", function() {
+//     $( ".cross" ).hide();
+//     $( ".hamburger" ).show();
+//   });
+// }
+// $( ".cross" ).click(closeMenu);
+//
+//
+// $( ".menu li" ).click(closeMenu);
 
 
 //
@@ -33,16 +33,16 @@ $( ".menu li" ).click(closeMenu);
 // 		alert("The sections have finished resizing");
 // 	}
 // });
-        var isAdded = false;
-        $(window).resize(function () {
-            if (!isAdded && ($(window).width() >= 450) && !($("h1").length)){
-                 isAdded = true;
-                 $('.text').prepend('<h1>pop.<span>Bubble.</span>pop</h1>');
-            } else if ( ($(window).width() <450)) {
-                isAdded = false;
-                $("h1").remove();
-            }
-        });
+        // var isAdded = false;
+        // $(window).resize(function () {
+        //     if (!isAdded && ($(window).width() >= 450) && !($("h1").length)){
+        //          isAdded = true;
+        //          $('.text').prepend('<h1>pop.<span>Bubble.</span>pop</h1>');
+        //     } else if ( ($(window).width() <450)) {
+        //         isAdded = false;
+        //         $("h1").remove();
+        //     }
+        // });
     });
 
 // });
