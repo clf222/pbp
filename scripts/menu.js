911544e6a4c0ac583df12145ec8291ed12ebeb36
Cindy Francies <clfrancies@comcast.net>
2017-10-20 16:33:32 -0400

    add mobile viewport width statementin header

--- scripts/menu.js
+++ scripts/menu.js
@@ -3,6 +3,7 @@ $( document ).ready(function() {
   	anchors: ['firstPage', 'secondPage', 'thirdPage', 'fourthPage', 'fifthPage'],
   	menu: '#myMenu',
     navigation: false,
+
     easing: 'easeInOutExpo',
 			scrollingSpeed: 1000,
 			css3: true,
