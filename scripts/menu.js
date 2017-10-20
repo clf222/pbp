60aa2a654c65875434d98487a10a9ad69f4acd2c
Cindy Francies <clfrancies@comcast.net>
2017-10-19 22:45:04 -0400

    script work

--- scripts/menu.js
+++ scripts/menu.js
@@ -2,46 +2,12 @@ $( document ).ready(function() {
   $('#fullpage').fullpage({
   	anchors: ['firstPage', 'secondPage', 'thirdPage', 'fourthPage', 'fifthPage'],
   	menu: '#myMenu',
-    navigation: false
+    navigation: false,
+    easing: 'easeInOutExpo',
+			scrollingSpeed: 1000,
+			css3: true,
+			controlArrows: false,
+			loopHorizontal: false,
+
   });
-
-//   $( ".cross" ).hide();
-//   $( ".menu" ).hide();
-//   $( ".hamburger" ).click(function() {
-//       $( ".menu" ).slideToggle( "slow", function() {
-//         $( ".hamburger" ).hide();
-//         $( ".cross" ).show();
-//       });
-//   });
-// function closeMenu() {
-//   $( ".menu" ).slideToggle( "slow", function() {
-//     $( ".cross" ).hide();
-//     $( ".hamburger" ).show();
-//   });
-// }
-// $( ".cross" ).click(closeMenu);
-//
-//
-// $( ".menu li" ).click(closeMenu);
-
-
-//
-// $('#fullpage').fullpage({
-// 	afterResize: function(){
-// 		var pluginContainer = $(this);
-// 		alert("The sections have finished resizing");
-// 	}
-// });
-        // var isAdded = false;
-        // $(window).resize(function () {
-        //     if (!isAdded && ($(window).width() >= 450) && !($("h1").length)){
-        //          isAdded = true;
-        //          $('.text').prepend('<h1>pop.<span>Bubble.</span>pop</h1>');
-        //     } else if ( ($(window).width() <450)) {
-        //         isAdded = false;
-        //         $("h1").remove();
-        //     }
-        // });
-    });
-
-// });
+});
